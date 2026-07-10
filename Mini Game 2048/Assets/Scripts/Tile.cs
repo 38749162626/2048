61b61e27fd3cfed44bc867a5c697398acd9e7c2a
38749162626 <3874916262@qq.com>
2026-07-11 04:09:00 +0800

    Canvas Setup

---
+++ Mini Game 2048/Assets/Scripts/Tile.cs
@@ -0,0 +1,25 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tile : MonoBehaviour
+{
+    public TileState state { get; private set; }
+    public TileCell cell { get; private set; }
+    public int number { get; private set; }
+
+    private Image background;
+    private TextMeshProUGUI text;
+
+    private void Awake()
+    {
+        background = GetComponent<Image>();
+        text = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    public void SetState(TileState state, int number)
+    {
+        this.state = state;
+        this.number = number;
+    }
+}
